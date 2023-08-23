b1e63a62ba0ff2b3d597e0cd65d963dc1ffcf5b6
enes <seyit7enes@gmail.com>
2023-08-23 04:41:10 +0200

    bug

--- src/src/firebase.tsx
+++ src/src/firebase.tsx
@@ -5,14 +5,15 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// firebase.js
 const firebaseConfig = {
-  apiKey: "AIzaSyDnxvYTn8Bv7Wq9siLlCSaQKkD_KT2Sd4E",
-  authDomain: "fir-auth-demo-20bab.firebaseapp.com",
-  projectId: "fir-auth-demo-20bab",
-  storageBucket: "fir-auth-demo-20bab.appspot.com",
-  messagingSenderId: "45341495580",
-  appId: "1:45341495580:web:9a6ba92af33ebb77552ad9",
-  measurementId: "G-PP43GNG76S",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
